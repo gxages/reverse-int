c6615f81ab1acf918f8bddfe16953980cb4ee0b7
Anton Papkou <101500746+gxages@users.noreply.github.com>
2023-01-29 18:54:31 +0100

    init:src

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+ 
+    let res = [...Math.abs(n).toString()].reverse().join("");
+    return Number(res);
+    // const num = toString(n);
+    // return num.split("").reverse().join("");
 }
+// const str = (Math.abs(num)).toString()
